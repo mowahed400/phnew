6b587b5fb85f66c8322bf8ae57b750ad9604ff68
mowahed400 <60117806+mowahed400@users.noreply.github.com>
2023-11-06 00:21:58 +0200

    test charts

--- resources/views/AdminPanel/app.blade.php
+++ resources/views/AdminPanel/app.blade.php
@@ -68,6 +68,7 @@ License: For each use you must have a valid license purchased only from above li
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.3/min/dropzone.min.js"></script>
     <link href="{{asset('assets/css/custom.css')}}" rel="stylesheet">
 
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 </head>
 <!--end::Head-->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,39 @@
 @extends('AdminPanel.app')
 @section('content')
 
+<canvas id="myChart" width="150" height="50"></canvas>
 
+
+
+<script>
+// Fetch your chart data (example data)
+const chartData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+        {
+            label: 'Sample Chart',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: true,
+            borderColor: 'rgba(75, 192, 192, 1)',
+            tension: 0.1
+        }
+    ]
+};
+
+// Get the canvas element
+const ctx = document.getElementById('myChart').getContext('2d');
+
+// Create the chart using Chart.js
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: chartData,
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+</script>
 @endsection
